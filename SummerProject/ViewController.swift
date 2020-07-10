2f8c2dd47fdaf18bdca0dee1e080af6468807a19
Nail <nail_ka_war@mail.ru>
2020-07-10 01:31:28 -0700

    add 3

---
+++ SummerProject/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  SummerProject
+//
+//  Created by Nail on 09.07.2020.
+//  Copyright © 2020 Nail. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    //3
+        
+}
+        
+        
+        
+        
+        
+        
+        
+        
+        
+      /*
+        super.viewDidLoad()
+         let plusButton = UIButton(frame: CGRect(x: 300, y: 450, width: 50, height: 50))
+        plusButton.setImage(UIImage(named: "plus1"), for: .normal)
+        self.view.addSubview(plusButton)
+        plusButton.addTarget(self, action: #selector(pressPlus), for: .touchUpInside)
+        
+        
+        
+        
+        
+        let secondPlusButton = UIButton(frame: CGRect(x: 300 , y: 350, width: 50, height: 50))
+         secondPlusButton.setImage(UIImage(named: "plus1"), for: .normal)
+        self.view.addSubview(secondPlusButton)
+        secondPlusButton.addTarget(self, action: #selector(pressSecond), for: .touchUpInside)
+    }
+        @objc func pressPlus(){
+            print("plus")
+        }
+        @objc func pressSecond(){
+        print("secondPlus")
+        */
+  
+        
+  
+    
+
